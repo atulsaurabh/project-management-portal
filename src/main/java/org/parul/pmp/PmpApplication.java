600c02aca6f6c701ca6e1269ef6bbdc0cd8091d6
atulsaurabh <atul.saurabh@thesunflowerlab.com>
2018-12-22 19:39:18 +0530

    initial commit

---
+++ src/main/java/org/parul/pmp/PmpApplication.java
@@ -0,0 +1,14 @@
+package org.parul.pmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmpApplication.class, args);
+	}
+
+}
+
