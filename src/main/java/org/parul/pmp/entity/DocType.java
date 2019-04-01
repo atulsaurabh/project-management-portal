9647c45cd34120a2b0286cfa9db5fbed2190016c
Reshma160303105720 <reshmace19@gmail.com>
2019-04-01 13:43:54 +0530

    relationship for uploading documents and tables done

---
+++ src/main/java/org/parul/pmp/entity/DocType.java
@@ -0,0 +1,39 @@
+package org.parul.pmp.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class DocType {
+    private long doctypeid;
+    private String documentname;
+    private Documents documents;
+
+    @OneToOne
+    @JoinColumn(name = "documentid")
+    public Documents getDocuments() {
+        return documents;
+    }
+
+    public void setDocuments(Documents documents) {
+        this.documents = documents;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long getDoctypeid() {
+        return doctypeid;
+    }
+
+    public void setDoctypeid(long doctypeid) {
+        this.doctypeid = doctypeid;
+    }
+
+
+    public String getDocumentname() {
+        return documentname;
+    }
+
+    public void setDocumentname(String documentname) {
+        this.documentname = documentname;
+    }
+}
---
+++ src/main/java/org/parul/pmp/entity/Documents.java
@@ -0,0 +1,62 @@
+package org.parul.pmp.entity;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.Date;
+
+@Entity
+public class Documents {
+    private long documentid;
+    private Instant startuploaddate;
+    private Instant enduploaddate;
+    private Department department;
+    private DocType docType;
+
+    @OneToOne(fetch = FetchType.EAGER,cascade = CascadeType.ALL,orphanRemoval = true)
+    @JoinColumn(name="doctypeid")
+    public DocType getDocType() {
+        return docType;
+    }
+
+    public void setDocType(DocType docType) {
+        this.docType = docType;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "deptid")
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long getDocumentid() {
+        return documentid;
+    }
+
+    public void setDocumentid(long documentid) {
+        this.documentid = documentid;
+    }
+
+    public Instant getStartuploaddate() {
+        return startuploaddate;
+    }
+
+    public void setStartuploaddate(Instant startuploaddate) {
+        this.startuploaddate = startuploaddate;
+    }
+
+    public Instant getEnduploaddate() {
+        return enduploaddate;
+    }
+
+    public void setEnduploaddate(Instant enduploaddate) {
+        this.enduploaddate = enduploaddate;
+    }
+}
