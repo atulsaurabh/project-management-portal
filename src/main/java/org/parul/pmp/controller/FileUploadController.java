3337f322ae16a01ff932ee80de527252207aaf9e
Reshma160303105720 <reshmace19@gmail.com>
2019-04-07 11:38:52 +0530

    file upload

--- src/main/java/org/parul/pmp/controller/FileUploadController.java
+++ src/main/java/org/parul/pmp/controller/FileUploadController.java
@@ -19,6 +19,7 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -27,6 +28,7 @@ import java.util.stream.Collectors;
 @Controller
 @RequestMapping("/uploadFile")
 public class FileUploadController {
+    private static String UPLOADED_FOLDER = "F://";
     @Autowired
     private DocumentRepository documentRepository;
     @Autowired
@@ -40,21 +42,23 @@ public class FileUploadController {
 
     @GetMapping
     public String uploadPage(Model model) {
-        List<DocType> documents = docTypeRepository.findAll();
+
+
+        /*List<DocType> documents = docTypeRepository.findAll();
         List<DocTypeDTO> doctypes = documents.stream().map(DocTypeMapper::toDTO).collect(Collectors.toList());
         model.addAttribute("doctypes", doctypes);
-        model.addAttribute("uploaddoc", new UploadDocDTO());
+        model.addAttribute("uploaddoc", new UploadDocDTO());*/
         return "fileUpload";
     }
 
-    @PostMapping
-    public String uploadDocument(@ModelAttribute("uploaddoc") UploadDocDTO uploaddoc, Model model, HttpSession session, CommonsMultipartFile[] fileUpload,DocType doctype) {
-        long userid = (long) session.getAttribute("userid");
-        Student student = studentRepository.findById(userid).get();
-        uploaddoc.setUploadedby(student.getEnrollment());
-        Documents documents = documentRepository.findByDocType(doctype).get();
+    @PostMapping("/upload")
+    public String uploadDocument(@RequestParam("file") MultipartFile file,Model model) {
+        //long userid = (long) session.getAttribute("userid");
+        //Student student = studentRepository.findById(userid).get();
+        //uploaddoc.setUploadedby(student.getEnrollment());
+        //Documents documents = documentRepository.findByDocType(doctype).get();
 
-        if (fileUpload != null && fileUpload.length > 0) {
+       /* if (fileUpload != null && fileUpload.length > 0) {
             for (CommonsMultipartFile file : fileUpload) {
 
                 System.out.println("Saving file: " + file.getOriginalFilename());
@@ -67,7 +71,26 @@ public class FileUploadController {
                 uploadDocRepository.saveAndFlush(uplodedDocuments);
             }
             model.addAttribute("msg","File Uploaded Successfully");
+        }*/
+        if (file.isEmpty()) {
+            model.addAttribute("msg", "Please select a file to upload");
+            return "fileUpload";
         }
+        try {
+
+            // Get the file and save it somewhere
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+
+            model.addAttribute("msg",
+                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return "fileUpload";
     }
+
 }
